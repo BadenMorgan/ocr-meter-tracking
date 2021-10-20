fb1aff02a50021dc3395c4ee7ef8c625af767a42
BadenMorgan <baden.david.morgan@gmail.com>
2021-10-20 23:27:59 +0200

    test new method

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ocr-meter-tracking
 An OCR approach to monitoring and tracking an electricity meter usage with a locally hosted webpage using a raspberry pi or similar
+
+install:
+sudo apt install libatlas3-base libsz2 libharfbuzz0b libtiff5 libjasper1 libilmbase12 libopenexr22 libilmbase12 libgstreamer1.0-0 libavcodec57 libavformat57 libavutil55 libswscale4 libqtgui4 libqt4-test libqtcore4
+
+sudo apt install tesseract-ocr
+
+sudo apt install libtesseract-dev
--- ocr_reader/main.py
+++ ocr_reader/main.py
@@ -8,8 +8,8 @@ sys.path.append('.')
 from PIL import Image
 import pytesseract
 import cv2 
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+# from picamera.array import PiRGBArray
+# from picamera import PiCamera
 
 #my libs
 
@@ -30,27 +30,43 @@ import sentry_sdk
 #         )
 
 # start_sentry()
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
 
-rawCapture = PiRGBArray(camera, size=(640, 480))
+import pytesseract
+from PIL import Image
+import cv2
 
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	image = frame.array
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
+img = cv2.imread('random_capture.png',cv2.IMREAD_COLOR) #Open the image from which charectors has to be recognized
+#img = cv2.resize(img, (620,480) ) #resize the image if required
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey to reduce detials
+gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+
+original = pytesseract.image_to_string(gray, config='')
+#test = (pytesseract.image_to_data(gray, lang=None, config='', nice=0) ) #get confidence level if required
+#print(pytesseract.image_to_boxes(gray))
+
+print (original)
+# camera = PiCamera()
+# camera.resolution = (640, 480)
+# camera.framerate = 30
+
+# rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+# 	image = frame.array
+# 	cv2.imshow("Frame", image)
+# 	key = cv2.waitKey(1) & 0xFF
 	
-	rawCapture.truncate(0)
+# 	rawCapture.truncate(0)
 
-	if key == ord("s"):
-		text = pytesseract.image_to_string(image)
-		print(text)
-		cv2.imshow("Frame", image)
-		cv2.waitKey(0)
-		break
+# 	if key == ord("s"):
+# 		text = pytesseract.image_to_string(image)
+# 		print(text)
+# 		cv2.imshow("Frame", image)
+# 		cv2.waitKey(0)
+# 		break
 
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     print(pytesseract.image_to_string(Image.open("random_capture.png")))
