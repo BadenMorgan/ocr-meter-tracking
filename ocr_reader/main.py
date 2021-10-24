ef0bbf5d5eb3a2eeb404e4b4238985706dfc759b
BadenMorgan <baden.david.morgan@gmail.com>
2021-10-24 18:37:59 +0200

    new image so I stop confusing myself

--- ocr_reader/main.py
+++ ocr_reader/main.py
@@ -31,7 +31,6 @@ import cv2
 
 # start_sentry()
 
-pytesseract.pytesseract.tesseract_cmd = r'/usr/include/'
 img = cv2.imread('random_capture.png',cv2.IMREAD_COLOR) #Open the image from which charectors has to be recognized
 #img = cv2.resize(img, (620,480) ) #resize the image if required
 
